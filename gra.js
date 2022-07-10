85b1ca2d8ed6b531ce0cbe03b61f0bc5ca63e2ec
Brosiatson <brosiatson@gmail.com>
2022-07-10 18:53:48 +0200

    updated game

--- gra.js
+++ gra.js
@@ -1,11 +1,20 @@
 const SCORE = document.querySelector(".iloscpkt")
 const CLICKER = document.querySelector(".kliker")
+const WEAPON = document.querySelectorAll(".ak47img")
 
 let score = localStorage.getItem("score") || "0"
 
 SCORE.innerText = localStorage.getItem("score")
 
 CLICKER.addEventListener("click", () => {
+    for (i=0; i < WEAPON.length; i++){
+        WEAPON[i].classList.add("ak47imgshot")
+    }
+    setTimeout(() => {
+        for (i=0; i < WEAPON.length; i++){
+            WEAPON[i].classList.remove("ak47imgshot")
+        }
+    }, 100)
     score++;
     localStorage.setItem("score", score)
     SCORE.innerText = localStorage.getItem("score")
