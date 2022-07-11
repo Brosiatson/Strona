1f2640ad17c44d5ee161acfb93df531ed46be0eb
Brosiatson <brosiatson@gmail.com>
2022-07-11 10:40:23 +0200

    updategra2

--- gra.js
+++ gra.js
@@ -1,6 +1,7 @@
 const SCORE = document.querySelector(".iloscpkt")
 const CLICKER = document.querySelector(".kliker")
 const WEAPON = document.querySelectorAll(".ak47img")
+const FIRE = document.querySelectorAll(".plomien")
 
 let score = localStorage.getItem("score") || "0"
 
@@ -9,10 +10,12 @@ SCORE.innerText = localStorage.getItem("score")
 CLICKER.addEventListener("click", () => {
     for (i=0; i < WEAPON.length; i++){
         WEAPON[i].classList.add("ak47imgshot")
+        FIRE[i].classList.add("plomienshot")
     }
     setTimeout(() => {
         for (i=0; i < WEAPON.length; i++){
             WEAPON[i].classList.remove("ak47imgshot")
+            FIRE[i].classList.remove("plomienshot")
         }
     }, 100)
     score++;
